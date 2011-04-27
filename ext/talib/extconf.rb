c64fd7ca0b571b1fad2e37087a3da0bbfcc7a965
metaphalon <metaphalon@gmail.com>
2011-04-28 00:19:30 +0200

    modified extconf.rb to search for lib and includes

--- ext/talib/extconf.rb
+++ ext/talib/extconf.rb
@@ -1,6 +1,5 @@
 require 'mkmf'
 
-dir_config("talib", "/home/www/ruby/include/ta-lib", "/home/www/ruby/lib")
-
-have_library("ta_lib", "TA_Initialize")
+raise "no talib.la" unless have_library("ta_lib", "TA_Initialize")
+raise "no ta_libc.h" unless have_header("ta_libc.h")
 create_makefile("talib")
