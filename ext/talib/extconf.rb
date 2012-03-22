e4c0b70982f92f9166cfa9347a0f89bcb251616f
Ray Ng <rayngwf@gmail.com>
2012-03-22 17:43:52 +0800

    update default header and include lib

--- ext/talib/extconf.rb
+++ ext/talib/extconf.rb
@@ -1,5 +1,20 @@
 require 'mkmf'
-dir_config("talib")
+
+HEADER_DIRS = [
+  '/opt/local/include',
+  '/usr/local/include',
+  '/usr/local/include/ta-lib',
+  Config::CONFIG['includedir'],
+  '/usr/include'
+]
+LIB_DIRS = [
+  '/opt/local/lib',
+  '/usr/local/lib',
+  Config::CONFIG['libdir'],
+  '/usr/lib',
+]
+
+dir_config("talib", HEADER_DIRS, LIB_DIRS)
 
 have_library("ta_lib", "TA_Initialize")
 create_makefile("talib")
